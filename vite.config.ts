0464d0ab67d9e71a850d818d8b9bae0ed2f2c59b
ashkuc <ashkucc@gmail.com>
2023-12-02 20:35:44 +0100

    fix build action

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // base: '/unique-magic-sample/',
-  base: process.env.NODE_ENV === 'production' ? '/unique-magic-sample/' : '/',
+  base: '/unique-magic-sample/',
 })
